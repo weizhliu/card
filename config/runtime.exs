31a1bc2091285e8d2cdbfb985c9fa07680c79a03
Weizheng Liu <wi6@me.com>
2021-11-05 02:30:45 +0800

    setup deploy to fly.io

--- config/runtime.exs
+++ config/runtime.exs
@@ -18,15 +18,17 @@ if config_env() == :prod do
       environment variable SECRET_KEY_BASE is missing.
       You can generate one by calling: mix phx.gen.secret
       """
+  app_name =
+    System.get_env("FLY_APP_NAME") ||
+      raise "FLY_APP_NAME not available"
 
   config :card, CardWeb.Endpoint,
+    server: true,
+    url: [host: "#{app_name}.fly.dev", port: 80],
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: String.to_integer(System.get_env("PORT") || "4000")
+      port: String.to_integer(System.get_env("PORT") || "4000"),
+      # IMPORTANT: support IPv6 addresses
+      transport_options: [socket_opts: [:inet6]]
     ],
     secret_key_base: secret_key_base
 
@@ -40,7 +42,7 @@ if config_env() == :prod do
   # Then you can assemble a release by calling `mix release`.
   # See `mix help release` for more information.
 
-  config :card, CardWeb.Endpoint, server: true
+  # config :card, CardWeb.Endpoint, server: true
 
   # ## Configuring the mailer
   #
