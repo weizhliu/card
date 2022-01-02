1039e3c0bc5f3caa38e0f0cff13fc1be951362bc
Weizheng Liu <wi6@me.com>
2022-01-02 13:22:38 +0800

    move host

--- config/runtime.exs
+++ config/runtime.exs
@@ -19,13 +19,13 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  app_name =
-    System.get_env("FLY_APP_NAME") ||
-      raise "FLY_APP_NAME not available"
+  url =
+    System.get_env("PHX_HOST") ||
+      raise "PHX_HOST not available"
 
   config :card, CardWeb.Endpoint,
     server: true,
-    url: [host: "#{app_name}.fly.dev", port: 80],
+    url: [host: url, port: 80],
     http: [
       port: String.to_integer(System.get_env("PORT") || "4000"),
       # IMPORTANT: support IPv6 addresses
